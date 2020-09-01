9e362a0e0acb60972d798ee24b85b84d70e2e847
Fahadayyoub <30391844+Fahadayyoub@users.noreply.github.com>
2020-09-01 08:09:42 +0500

    Add files via upload

---
+++ precache-manifest.208d12bf8bcd4110e7fc7800ba49de07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "371a672b7e6a8f0d51df821c8fdb91ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bd8a324c1e7c704aa12",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "a885376139ecc2765d8d",
+    "url": "/static/css/main.e85432ec.chunk.css"
+  },
+  {
+    "revision": "6bd8a324c1e7c704aa12",
+    "url": "/static/js/2.c6bc0726.chunk.js"
+  },
+  {
+    "revision": "af5492f10234ec8ba29a624c4fe6fb11",
+    "url": "/static/js/2.c6bc0726.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a885376139ecc2765d8d",
+    "url": "/static/js/main.a248a1f9.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
